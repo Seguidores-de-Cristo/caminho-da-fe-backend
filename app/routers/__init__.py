4cf5d42f28871a80630c0ad928b5eced1d4b4714
Ademir Mendonça <ademir_mendonca@hotmail.com>
2025-12-17 08:49:47 -0300

    Segregação das rotas

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,9 +3,11 @@
 from app.routers.users_router import router as users_router
 from app.routers.novos_convertidos_router import router as novos_convertidos_router
 from app.routers.discipulado_router import router as discipulado_router
+from app.routers.cep_router import router as cep_router
 
 __all__ = [
     "users_router",
     "novos_convertidos_router",
     "discipulado_router",
+    "cep_router",
 ]
