39d0a43ed36b96b58e709822ec4dd7a9954851d3
willianns <775423+willnss@users.noreply.github.com>
2025-12-20 18:53:08 +0000

    ajustes

--- alembic/versions/b043f441bc13_contatos_novo_convertido_acoes.py
+++ alembic/versions/b043f441bc13_contatos_novo_convertido_acoes.py
@@ -9,7 +9,6 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
 revision: str = 'b043f441bc13'
@@ -19,48 +18,19 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        "contato_novo_convertido_acoes",
-        sa.Column("id", sa.Integer(), primary_key=True, nullable=False),
-
-        sa.Column(
-            "contato_novo_convertido_id",
-            sa.Integer(),
-            sa.ForeignKey("contatoNovoConvertido.id"),
-            nullable=False,
-        ),
-
-        sa.Column("convite_culto_igreja", sa.Boolean(), nullable=False),
-        sa.Column("convite_culto_lar", sa.Boolean(), nullable=False),
-        sa.Column("convite_escola_dominicial", sa.Boolean(), nullable=False),
-        sa.Column("convite_reuniao_discipulado", sa.Boolean(), nullable=False),
-        sa.Column("teleoracao", sa.Boolean(), nullable=False),
-
-        sa.Column("outros_especificar", sa.Boolean(), nullable=False),
-        sa.Column("especificacao_outros", sa.String(length=1000), nullable=True),
-
-        sa.Column("convite_culto_igreja_resposta", sa.Boolean(), nullable=False),
-        sa.Column("convite_culto_lar_resposta", sa.Boolean(), nullable=False),
-        sa.Column("convite_escola_dominicial_resposta", sa.Boolean(), nullable=False),
-        sa.Column("convite_reuniao_discipulado_resposta", sa.Boolean(), nullable=False),
-
-        sa.Column("outros_especificar_resposta", sa.Boolean(), nullable=False),
-        sa.Column("especificacao_outros_resposta", sa.String(length=1000), nullable=True),
-
-        sa.Column("manter_contato", sa.Boolean(), nullable=False),
-
-        mysql_engine="InnoDB",
-        mysql_default_charset="utf8mb4",
-    )
-
-    op.create_index(
-        "ix_contato_novo_convertido_acoes_id",
-        "contato_novo_convertido_acoes",
-        ["id"],
-    )
-
-
+    op.add_column('contato_novo_convertido_acoes', sa.Column('convite_culto_igreja_resposta', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('convite_culto_lar_resposta', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('convite_escola_dominicial_resposta', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('convite_reuniao_discipulado_resposta', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('outros_especificar_resposta', sa.Boolean(), nullable=False, server_default=sa.false()))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('especificacao_outros_resposta', sa.String(length=1000), nullable=True))
+    op.add_column('contato_novo_convertido_acoes', sa.Column('manter_contato', sa.Boolean(), nullable=False, server_default=sa.false()))
 
 def downgrade() -> None:
-    op.drop_table("contato_novo_convertido_acoes")
-
+    op.drop_column('contato_novo_convertido_acoes', 'convite_culto_igreja_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'convite_culto_lar_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'convite_escola_dominicial_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'convite_reuniao_discipulado_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'outros_especificar_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'especificacao_outros_resposta')
+    op.drop_column('contato_novo_convertido_acoes', 'manter_contato')
